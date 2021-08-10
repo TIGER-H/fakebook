ee952e7e3a9d257d772c138bdf380a917a5f5e7a
yeoreum <549747168@qq.com>
2021-08-10 21:49:57 +0800

    loading

---
+++ client/src/components/loading/Loading.jsx
@@ -0,0 +1,37 @@
+import "./loading.css";
+
+export const Loading = ({ type }) => {
+  const COUNT = 8;
+  const TopSkeleton = () => {
+    <div className="topSk">
+      <div className="topSkMiddle"></div>
+      <div className="topSkMiddle"></div>
+      <div className="topSkUser"></div>
+      <div className="topSkIcon"></div>
+      <div className="topSkIcon"></div>
+      <div className="topSkIcon"></div>
+    </div>;
+  };
+  const MenuSkeleton = () => (
+    <div className="menuSk">
+      <div className="menuSkItem"></div>
+    </div>
+  );
+  const Balls = () => (
+    <div className="ballsLoading">
+      <div className="balls">
+        <div className="ball ball1"></div>
+        <div className="ball ball2"></div>
+        <div className="ball ball3"></div>
+      </div>
+      <span className="loadingText">Loading...</span>
+    </div>
+  );
+
+  switch (type) {
+    case "balls":
+      return <Balls />;
+    case "menusk":
+      return Array(COUNT).fill(<MenuSkeleton />);
+  }
+};
